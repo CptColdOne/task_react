338e15e9ffdea86f2210d8a7fcb03263d5e46cb2
panferov-2010 <panferov-2010@mail.ru>
2021-10-25 15:32:34 +0600

    Add buttons add and remove elements

--- src/List.js
+++ src/List.js
@@ -3,16 +3,14 @@ import React, {Component} from "react";
 export default class List extends Component{
     constructor(){
         super();
-        this.setState({list: [{title: "First"}, {title: "Second"}]})
+        this.state = {
+            list: [{title: "First"}, {title: "Second"}],
+            addCount: 0,
+            removeCount: 0
+        };
         
     }
-    state = {
-        list: [],
-        addCount: 0,
-        removeCount: 0
-    };
-
-
+    
     renderListItem({item, index}){
         return <div key={item.title}>{item.title}</div>;
     }
@@ -28,8 +26,8 @@ export default class List extends Component{
     }
 
     addElem= () =>{
-
-        this.setState({addCount: ++this.state.addCount});
+        this.state.list.push({title: Math.random()});
+        this.setState({addCount: this.state.addCount + 1});
     }
 
     renderButtonRemove(){
@@ -37,8 +35,8 @@ export default class List extends Component{
     }
 
     removeElem= () =>{
-
-        this.setState({removeCount: ++this.state.removeCount});
+        this.state.list.pop();
+        this.setState({removeCount: this.state.removeCount + 1});
     }
 
     render(){
